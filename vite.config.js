78f9d322b0a708da97d95627518259b63a6542aa
LuuVanSonn <sonlvph20306@fpt.edu.vn>
2022-06-01 08:38:13 +0700

    capnhat

--- vite.config.js
+++ vite.config.js
@@ -8,8 +8,8 @@ module.exports = defineConfig({
         main: resolve(__dirname, 'index.html'),
         detail: resolve(__dirname, 'detail.html'),
         products: resolve(__dirname, 'products.html'),
-        signin: resolve(__dirname, 'signin.html'),
-        signup: resolve(__dirname, 'signup.html'),
+        signin: resolve(__dirname, 'sign-in.html'),
+        signup: resolve(__dirname, 'sign-up.html'),
 
       }
     }
